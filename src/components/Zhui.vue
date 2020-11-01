5d7d9aba58c200dd351b59542ad56ac4b19db2ef
enobj <laoji52125@163.com>
2020-11-01 21:09:00 +0800

    能显示最近消息时间和切换通知开关

--- src/components/Zhui.vue
+++ src/components/Zhui.vue
@@ -2,10 +2,28 @@
   <div>
     <p>所有追更</p>
     <div v-for="userChannel in userChannels" v-bind:key="userChannel._id">
-      <router-link
-        v-bind:to="'/channel/' + userChannel.channel._id"
-        >{{ userChannel.channel.name }}-{{userChannel.channel.channelTemplate.name}}</router-link
+      <router-link v-bind:to="'/channel/' + userChannel.channel._id"
+        >{{ userChannel.channel.name }}-{{
+          userChannel.channel.channelTemplate.name
+        }}</router-link
       >
+      <div v-if="userChannel.channelDataMessage">
+        {{ userChannel.channelDataMessage.createTime | formatPass }}
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          v-bind:id="userChannel._id"
+          v-model="userChannel.notify"
+          v-bind:disabled="!cloudAuth.currentUser.email"
+          v-on:change="switchNotify(userChannel._id, $event)"
+        />
+        <label v-bind:for="userChannel._id"
+          >邮件通知<router-link v-show="!cloudAuth.currentUser.email" to="/more"
+            >（此操作需要登录）</router-link
+          ></label
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -20,17 +38,45 @@ export default {
     };
   },
   mounted: function () {
+    const db = this.cloud.database();
     // 加载频道
-      CloudUtils.getAll(
-        this.cloud
-          .database()
-          .collection("ty_user_channel")
-          .where({})
-          .orderBy("top", "desc")
-          .orderBy("updateTime", "desc")
-      ).then((list) => {
-        this.userChannels = list;
-      });
+    CloudUtils.getAll(
+      db
+        .collection("ty_user_channel")
+        .where({})
+        .orderBy("top", "desc")
+        .orderBy("updateTime", "desc")
+    ).then((list) => {
+      this.userChannels = list;
+      // 加载消息
+      this.userChannels.forEach(
+        function (userChannel) {
+          db.collection("ty_user_channel_data_message")
+            .where({
+              "channelData.channel._id": userChannel.channel._id,
+            })
+            .orderBy("createTime", "desc")
+            .limit(1)
+            .get()
+            .then(
+              function (res) {
+                this.$set(userChannel, `channelDataMessage`, res.data[0]);
+              }.bind(this)
+            );
+        }.bind(this)
+      );
+    });
+  },
+  methods: {
+    switchNotify: function (userChannelId, event) {
+      this.cloud
+        .database()
+        .collection("ty_user_channel")
+        .doc(userChannelId)
+        .update({
+          notify: event.target.checked,
+        });
+    },
   },
 };
 </script>
