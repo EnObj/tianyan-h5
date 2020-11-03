d66e0c674914eaa75f5de2a4244020fd34cfdfa6
enobj <laoji52125@163.com>
2020-11-03 16:05:15 +0800

    首页样式搞定

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,123 @@
 <template>
   <div id="app">
-    <p>
-      <router-link to="/zhui">追更</router-link>
-      <router-link to="/explore">探索</router-link>
-      <router-link to="/more">更多</router-link>
-    </p>
+    <nav class="top-bar" data-topbar>
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="#">爱追更</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon">
+          <a href="#"><span></span></a>
+        </li>
+      </ul>
+
+      <section class="top-bar-section">
+        <ul class="left">
+          <li class="active"><router-link to="/zhui">追更</router-link></li>
+          <li><router-link to="/explore">探索</router-link></li>
+          <li><router-link to="/more">更多</router-link></li>
+        </ul>
+      </section>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
+.flex-between {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.flex-start {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.flex-end {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.flex-around {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.padding-box {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background: #fff;
+}
+
+.padding-box-no-bg {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.not-importent {
+  color: gray;
+  font-size: 14px;
+}
+
+.abs-center {
+  display: flex;
+}
+.abs-center > .abs-center-content {
+  margin: auto;
+}
+
+.no-wrap {
+  max-width: 100%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.ref-content {
+  color: #576b95;
+}
+
+.success-content {
+  color: #09bb07;
+}
+
+.nav-none-bg:active {
+  background: none;
+}
+
+.active-hover:active {
+  background: #eee;
+}
+
+.bg {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+}
+
+.bg-img {
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.2;
+}
+
 #app {
-  padding: 15px;
+  background: #f6f6f6;
+  min-height: 100vh;
 }
 </style>
--- src/components/Zhui.vue
+++ src/components/Zhui.vue
@@ -1,35 +1,50 @@
 <template>
   <div>
-    <div v-if="userChannels.length">
-      <div v-for="userChannel in userChannels" v-bind:key="userChannel._id">
-        <router-link v-bind:to="'/channel/' + userChannel.channel._id">{{
-          userChannel.channel.name
-        }}</router-link
-        >-<router-link
-          v-bind:to="
-            '/channel-template/' + userChannel.channel.channelTemplate._id
-          "
-          >{{ userChannel.channel.channelTemplate.name }}</router-link
-        >
-        <div v-if="userChannel.channelDataMessage">
-          {{ userChannel.channelDataMessage.createTime | formatPass }}
-          <span v-show="!userChannel.channelDataMessage.readed">新</span>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            v-bind:id="userChannel._id"
-            v-model="userChannel.notify"
-            v-bind:disabled="!cloudAuth.currentUser.email"
-            v-on:change="switchNotify(userChannel._id, $event)"
-          />
-          <label v-bind:for="userChannel._id"
-            >邮件通知<router-link
-              v-show="!cloudAuth.currentUser.email"
-              to="/more"
-              >（此操作需要登录）</router-link
-            ></label
+    <div v-if="userChannels.length" class="user-channels">
+      <div
+        v-for="userChannel in userChannels"
+        v-bind:key="userChannel._id"
+        class="user-channel padding-box flex-between"
+        v-bind:style="{
+          borderLeftColor:
+            userChannel.channel.channelTemplate.mainColor || 'gray',
+        }"
+      >
+        <div class="flex-start" style="flex: auto; overflow: hidden">
+          <div class="channel-name no-wrap">
+            <router-link v-bind:to="'/channel/' + userChannel.channel._id">{{
+              userChannel.channel.name
+            }}</router-link>
+          </div>
+          <div
+            v-if="userChannel.channelDataMessage"
+            v-show="!userChannel.channelDataMessage.readed"
+            class="unreaded"
+            v-bind:style="{
+              background:
+                userChannel.channel.channelTemplate.mainColor || 'gray',
+            }"
           >
+            <span>新</span>
+          </div>
+        </div>
+        <div
+          style="flex: none"
+          class="notify"
+          v-bind:class="{ 'notify-on': userChannel.notify }"
+        >
+          <div
+            v-if="cloudAuth.currentUser.email"
+            v-on:click="
+              userChannel.notify = !userChannel.notify;
+              switchNotify(userChannel._id, userChannel.notify);
+            "
+          >
+            <i class="fi-mail"></i>
+          </div>
+          <div v-else v-on:click="$router.push('/more')">
+            <i class="fi-mail">请登录</i>
+          </div>
         </div>
       </div>
     </div>
@@ -85,15 +100,84 @@ export default {
     });
   },
   methods: {
-    switchNotify: function (userChannelId, event) {
+    switchNotify: function (userChannelId, notify) {
       this.cloud
         .database()
         .collection("ty_user_channel")
         .doc(userChannelId)
         .update({
-          notify: event.target.checked,
+          notify,
         });
     },
   },
 };
 </script>
+
+<style scoped>
+.explore {
+  height: 100px;
+}
+.unreaded {
+  padding: 2px 10px;
+  background: red;
+  border-radius: 10px;
+  margin-left: 5px;
+  color: white;
+  font-size: 10px;
+}
+.user-channels {
+  /* padding: 15px; */
+  padding-bottom: 1px;
+}
+.user-channel {
+  max-width: 100%;
+  box-sizing: border-box;
+  position: relative;
+  overflow: hidden;
+  border-left: gray 10px solid;
+  border-radius: 10px;
+  padding: 15px 10px;
+}
+.channel-name {
+  margin-left: 5px;
+}
+.notify {
+  padding: 0 10px 0 15px;
+  font-size: 20px;
+  color: #ddd;
+}
+.notify-on {
+  color: #008cba;
+}
+.channel-template {
+  margin-top: 5px;
+}
+
+.top {
+  /* border-left: 3px #07c160 solid; */
+  position: absolute;
+  right: -11px;
+  top: -11px;
+  width: 20px;
+  height: 20px;
+  transform: rotate(45deg);
+  background: #aaa;
+}
+
+.empty-tip {
+  height: 80vh;
+}
+
+.door {
+  padding: 10px 30px;
+  background: #07c160;
+  border-radius: 20px;
+  margin-top: 20px;
+  color: #fff;
+}
+
+.message {
+  flex: none;
+  margin-left: 10px;
+}
+</style>
