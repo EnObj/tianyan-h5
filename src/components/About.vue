fb6b5e0e03b226720ca703bbc9dfa9288e89c9f4
enobj <laoji52125@163.com>
2020-11-04 11:12:57 +0800

    关于页面优化

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>
-        <ul>
-            <li>第1章 爱 在茫茫网络世界中，你发现了真爱，TA不定期发布动态</li>
-            <li>第2章 追 订阅TA的频道，此小程序帮你实时追踪</li>
-            <li>第3章 更 TA更新动态，你第一时间收到通知，把爱的首赞及留言放在前排</li>
-        </ul>
-        <router-link to="/explore">现在就去找到TA</router-link>
+  <div class="about">
+    <h1>关于爱追更</h1>
+    <h2>第1章 爱</h2>
+    <p>在茫茫网络世界中，你发现了真爱，TA不定期发布动态</p>
+    <h2>第2章 追</h2>
+    <p>订阅TA的频道，此小程序帮你实时追踪</p>
+    <h2>第3章 更</h2>
+    <p>TA更新动态，你第一时间收到通知，把爱的首赞及留言放在前排</p>
+    <div style="margin-top:50px;">
+        <button v-on:click="$router.push('/explore')" class="button expand round">现在就去找到TA</button>
     </div>
+  </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 10px 20px;
+}
+</style>
