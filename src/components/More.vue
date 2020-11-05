db83c3b39b3690a8329c00f32d7686fe639fdba4
enobj <laoji52125@163.com>
2020-11-05 23:28:17 +0800

    消息badge

--- src/components/More.vue
+++ src/components/More.vue
@@ -17,14 +17,16 @@
         </div>
       </div>
       <div class="login-area">
-        <el-button-group v-if="user.loginType == 'ANONYMOUS'"
-          key="no-eamil">
-          <el-button type="primary" v-on:click="$router.push('/sign-in')">登录</el-button>
-          <el-button type="primary" v-on:click="$router.push('/sign-up')">注册</el-button>
+        <el-button-group v-if="user.loginType == 'ANONYMOUS'" key="no-eamil">
+          <el-button type="primary" v-on:click="$router.push('/sign-in')"
+            >登录</el-button
+          >
+          <el-button type="primary" v-on:click="$router.push('/sign-up')"
+            >注册</el-button
+          >
         </el-button-group>
-        <el-button-group v-else
-          key="with-eamil">
-          <el-button type="danger" v-on:click="signOut">退出登录</el-button>
+        <el-button-group v-else key="with-eamil">
+          <el-button type="info" v-on:click="signOut">退出登录</el-button>
         </el-button-group>
         <!-- <ul
           class="button-group round"
@@ -44,10 +46,19 @@
       </div>
     </div>
     <p>
-      <el-link v-on:click="$router.push('/message-box')" type="primary">消息<span class="label alert round" v-show="lastUnreadedMessage">新</span></el-link>
+      <el-badge v-bind:value="'新'" class="item" v-bind:hidden="!lastUnreadedMessage">
+        <el-link v-on:click="$router.push('/message-box')" type="primary"
+          >消息</el-link
+        >
+      </el-badge>
     </p>
     <p>
-      <el-link href="https://support.qq.com/product/290950" target="_blank" type="primary">反馈</el-link>
+      <el-link
+        href="https://support.qq.com/product/290950"
+        target="_blank"
+        type="primary"
+        >反馈</el-link
+      >
     </p>
     <p>
       <el-link v-on:click="$router.push('/about')" type="primary">关于</el-link>
@@ -61,23 +72,30 @@ export default {
   data: function () {
     return {
       user: this.cloudAuth.currentUser,
-      lastUnreadedMessage: null
+      lastUnreadedMessage: null,
     };
   },
-  mounted(){
+  mounted() {
     // 查询最新一条未读消息
-    this.cloud.database().collection('ty_user_channel_data_message').where({
-      readed: false
-    }).orderBy('createTime', 'desc').limit(1).get().then(res=>{
-      this.lastUnreadedMessage = res.data[0] || null
-    })
+    this.cloud
+      .database()
+      .collection("ty_user_channel_data_message")
+      .where({
+        readed: false,
+      })
+      .orderBy("createTime", "desc")
+      .limit(1)
+      .get()
+      .then((res) => {
+        this.lastUnreadedMessage = res.data[0] || null;
+      });
   },
   methods: {
     signIn: function () {
       const email = prompt("请输入邮箱");
-      if(email){
+      if (email) {
         const password = prompt("请输入密码");
-        if(password){
+        if (password) {
           this.cloudAuth
             .signInWithEmailAndPassword(email, password)
             .then((item) => {
@@ -86,7 +104,7 @@ export default {
             })
             .catch((error) => {
               console.error("登录失败", error);
-              alert('邮箱或密码错误')
+              alert("邮箱或密码错误");
             });
         }
       }
@@ -101,7 +119,7 @@ export default {
     },
     updateEmail: function () {
       const email = prompt("请输入登录邮箱");
-      if(email){
+      if (email) {
         return this.user
           .updateEmail(email)
           .then(() => {
@@ -120,7 +138,7 @@ export default {
         return Promise.resolve();
       }
       const password = prompt("请设置登录密码");
-      if(password){
+      if (password) {
         return this.cloudAuth.currentUser
           .updatePassword(password)
           .then(() => {
@@ -131,7 +149,7 @@ export default {
             return Promise.reject();
           });
       }
-      return Promise.reject()
+      return Promise.reject();
     },
     updateUsername: function () {
       return this.cloudAuth.currentUser
@@ -178,7 +196,7 @@ export default {
   margin: 10px auto;
   color: gray;
 }
-.login-area{
-  margin: 20px 0 10px;
+.login-area {
+  margin: 20px 0;
 }
 </style>
