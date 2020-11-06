2243a3312a713818ddb5a2ac28a8c015a4ff8356
enobj <laoji52125@163.com>
2020-11-06 12:15:55 +0800

    修复配置错误

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    configureWebpack: {
-      plugins: [
-        new HtmlWebpackPlugin({title: '爱追更 - 追你所爱'})
-      ]
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = '爱追更 - 追你所爱'
+                return args
+            })
     }
-  }
+}
