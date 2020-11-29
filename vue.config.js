788860143beef9e12fb2de84c679f4c6d5549eb1
enobj <laoji52125@163.com>
2020-11-29 15:42:44 +0800

    使用tcb托管dns

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: "",
+    publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://6465-dev-9g0suwuw61afb9f3-1252108641.tcb.qcloud.la/enobj/tianyan/'
+    : '/',
     chainWebpack: config => {
         config
             .plugin('html')
