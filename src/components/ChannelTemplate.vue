f37f568220667878b15896f232ff50f679ceccaa
aolihui <laoji52125@163.com>
2022-11-23 17:16:08 +0800

    优化频道页

--- src/components/ChannelTemplate.vue
+++ src/components/ChannelTemplate.vue
@@ -1,112 +1,61 @@
 <template>
   <div v-if="channelTemplate" class="channel-template">
-    <el-breadcrumb style="margin-top:20px;">
+    <el-breadcrumb style="margin-top: 20px">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>节点</el-breadcrumb-item>
     </el-breadcrumb>
     <h1 class="my-4 text-lg">{{ channelTemplate.name }}</h1>
     <p>{{ channelTemplate.desc }}</p>
-    <div v-if="channelTemplate.keyName">
-      <div style="margin-top:30px;">
-        <el-input
-          v-bind:placeholder="'请输入' + channelTemplate.keyName"
-          v-model="keyword"
-          clearable
-          prefix-icon="el-icon-search"
-          v-on:change="search(keyword)"
-        />
-      </div>
-      <div v-if="advices.length">
-        <p class="not-importent">建议</p>
-        <!-- <div v-for="advice in advices" v-bind:key="advice">
-          <button v-on:click="search(advice)">{{ advice }}</button>
-        </div> -->
-        <div
-          v-for="advice in advices"
-          v-bind:key="advice"
-          v-on:click="search(advice)"
-          class="padding-box channel flex-start"
-        >
-          <ChannelTemplateLogo
-            v-bind:main-color="channelTemplate.mainColor"
-            v-bind:text="channelTemplate.name.substr(0, 1)"
-          />
-          <div>
-            <div>
-              <a>
-                {{ advice }}
-              </a>
-            </div>
-          </div>
-        </div>
+    <div class="popular-channels mt-4" v-show="popularChannels.length">
+      <div class="not-importent">最受欢迎</div>
+      <div
+        class="popular-channel"
+        v-for="channel in popularChannels"
+        v-on:click="$router.push('/channel/' + channel._id)"
+        :key="channel._id"
+      >
+        <ChannelCardVue :channel="channel"></ChannelCardVue>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ChannelTemplateLogo from "./ChannelTemplateLogo";
+import ChannelCardVue from "./ChannelCard.vue";
 export default {
   name: "ChannelTemplate",
   props: ["id"],
-  data: function() {
+  data: function () {
     return {
       channelTemplate: null,
       advices: [],
       keyword: "",
+      popularChannels: [],
     };
   },
-  components: { ChannelTemplateLogo },
-  mounted: function() {
+  components: { ChannelCardVue },
+  async mounted() {
     const db = this.cloud.database();
-    // 加载channel
-    db.collection("ty_channel_template")
-      .doc(this.id)
-      .get()
-      .then((res) => {
-        this.channelTemplate = res.data[0];
-      });
+    // 加载channel模板
+    const {
+      data: [channelTemplate],
+    } = await db.collection("ty_channel_template").doc(this.id).get();
+    this.channelTemplate = channelTemplate;
+
+    // 加载模板下最受欢迎频道
+    const { data: channels } = await this.cloud
+      .database()
+      .collection("ty_channel")
+      .where({
+        "channelTemplate._id": channelTemplate._id,
+        userChannelCount: this.cloud.database().command.gt(1),
+      })
+      .orderBy("userChannelCount", "desc")
+      .limit(20)
+      .get();
+    this.popularChannels = channels;
   },
-  methods: {
-    search: function(key) {
-      const loading = this.$loading({
-        lock: true,
-        text: "Loading",
-        spinner: "el-icon-loading",
-      });
-      this.cloud
-        .callFunction({
-          name: "resolveTyChannel",
-          data: {
-            templateId: this.channelTemplate._id,
-            key,
-          },
-        })
-        .then((res) => {
-          loading.close();
-          if (!res.result.errCode) {
-            this.$router.push("/channel/" + res.result.channel._id);
-          } else {
-            if (res.result.errCode == 405) {
-              this.advices = res.result.advices;
-            } else {
-              this.$message({
-                message: res.result.errMsg,
-                type: "warning",
-              });
-            }
-          }
-        })
-        .catch((err) => {
-          loading.close();
-          console.error(err);
-          this.$message({
-            message: "操作失败，请稍后重试",
-            type: "warning",
-          });
-        });
-    },
-  },
+  methods: {},
 };
 </script>
 
